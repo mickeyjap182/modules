b9b6438ed6860e4da5f7c3b0f2828ff743579028
mickeyjap182 <yoshitaka_8an9drums@msn.com>
2019-05-04 17:54:35 +0900

    add datahandler and csv module

---
+++ datahandler/csvfile.py
@@ -0,0 +1,82 @@
+import os
+import csv
+
+class Formatter():
+    DEL_COM = ','
+    LINE_LF = '\n'
+    UTF_8 = 'utf-8'
+    DBL_QUOTE = '"'
+
+    def __init__(self, *args, **kwargs):
+        """
+        delimiter: delimiter of column
+        line_sep: separater of line as record
+        """
+        self.delimiter = kwargs['delimiter'] if 'delimiter' in kwargs else Formatter.DEL_COM
+        self.quotechar = kwargs['quotechar'] if 'quotechar' in kwargs else Formatter.DBL_QUOTE
+        self.line_sep = kwargs['line_sep'] if 'line_sep' in kwargs else Formatter.LINE_LF
+        self.charset = kwargs['charset'] if 'charset' in kwargs else Formatter.UTF_8
+
+class Writer():
+    def __init__(self):
+        """
+        Not Implements
+        """
+        pass
+    def write(self, formatter):
+        pass
+
+class Reader():
+    def __init__(self):
+        """
+        Not Implements
+        """
+        pass
+
+
+class Csv():
+    def __init__(self, formatter=None):
+        """
+        delimiter: delimiter of column
+        line_sep: separater of line as record
+        """
+        if formatter is None:
+            self.form = Formatter()
+        else:
+            self.form = formatter
+
+
+    def write(self, file_path, contents, headers=None):
+        """ write file """
+        with open(file_path, 'w', newline=self.form.line_sep) as csv_file:
+            writer = csv.writer(
+                csv_file,
+                delimiter=self.form.delimiter,
+                quotechar=self.form.quotechar,
+                quoting=csv.QUOTE_MINIMAL
+            )
+            if headers is not None:
+                writer.writerow(headers)
+            for content in contents:
+                writer.writerow(content)
+        return True
+
+
+
+
+    def read(self, file_path, headers=None):
+        """ read file """
+        contents = []
+        with open(file_path, "r", newline=self.form.line_sep) as csv_file:
+            reader = csv.reader(
+                csv_file,
+                delimiter=self.form.delimiter,
+                quotechar=self.form.quotechar,
+                quoting=csv.QUOTE_MINIMAL
+            )
+            for n, row in enumerate(reader):
+                if headers is not None and n == 0 :
+                     continue
+                contents.append(row)
+
+        return contents
---
+++ tests/datahandler/csvfile.py
@@ -0,0 +1,51 @@
+import os, sys, unittest
+
+module_path  = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+
+class TestCsv(unittest.TestCase):
+
+    def setUp(self):
+        self.in_path = os.path.join(module_path, 'tests', 'datahandler', 'input')
+        self.out_path = os.path.join(module_path, 'tests', 'datahandler', 'output')
+        out_files = self._output_files()
+        ret = [os.remove(file) for file in out_files]
+
+    def tearDown(self):
+        pass
+
+    def test_file_format(self):
+        f = Formatter()
+
+        self.assertEqual(f.delimiter,',')
+        self.assertEqual(f.line_sep,'\n')
+        self.assertEqual(f.charset,'utf-8')
+
+    def test_read_file(self):
+        f = Formatter()
+        c = Csv(f)
+        headers = ['no', 'first_name', 'family_name']
+        contents = c.read(os.path.join(self.in_path, 'comma_sep.csv'), headers=headers)
+        # print(contents)
+        self.assertEqual(4,len(contents))
+
+    def test_write_file(self):
+        f = Formatter()
+        c = Csv(f)
+        headers = ['rownum', 'code', 'name']
+        contents = [
+            [1,'C_001', "Apple"],
+            [2,'C_002', "Banana"],
+            [3,'C_002', "Cytras"],
+        ]
+        ret = c.write(os.path.join(self.out_path, 'has_header.csv'), contents, headers=headers)
+        # print(contents)
+        self.assertTrue(ret)
+
+    def _output_files(self):
+        return [os.path.join(self.out_path, name) for name in os.listdir(self.out_path) if os.path.isfile(os.path.join(self.out_path, name)) and name != '.gitkeep']
+
+if __name__ == '__main__':
+    if module_path not in sys.path:
+        sys.path.append(module_path)
+    from datahandler.csvfile import Formatter, Csv
+    unittest.main()
