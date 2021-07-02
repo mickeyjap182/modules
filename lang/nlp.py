41d9acc6282e6c2a2c4089607d18d30d5c4fe551
mickeyjap182 <yoshitaka_8an9drums@msn.com>
2021-07-03 00:01:06 +0900

    add files

--- lang/nlp.py
+++ lang/nlp.py
@@ -2,17 +2,20 @@ from ginza import *
 import spacy
 
 class Processing():
-
+    DEFAULT_LANG = 'ja_ginza'
     def __init__(self, *args, **kwargs):
         """
         
         """
-        pass
+        lang = kwargs['lang'] if 'lang' in kwargs else Processing.DEFAULT_LANG 
+        self.nlp = spacy.load(lang) # Japanese lang model named GiNZA.
 
-    def example(self):
-        nlp = spacy.load("ja_ginza") # Japanese lang model named GiNZA.
-        doc = nlp("今日の午後は、突然降り出した雨の影響で、予定が大幅に狂ってしまった。")
-        print(doc.text)
-        for token in doc:
-            print(token.text, token.pos_, token.dep_)
-        return ''
+    def get_pipeline(self):
+        pipelines = []
+        for task in self.nlp.pipeline:
+            pipelines.append(task)
+        return pipelines
+
+
+    def analyze(self, word:str):
+        return self.nlp(word)
--- tests/lang/nlp.py
+++ tests/lang/nlp.py
@@ -10,9 +10,26 @@ class ProcessingTest(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_example(self):
+    def test_pipeline(self):
+        p = Processing(Processing.DEFAULT_LANG)
+        pipelines = p.get_pipeline()
+        self.assertEqual('tagger', pipelines[0][0]) # part of speech tagging
+        self.assertEqual('parser', pipelines[1][0]) # dependency parsing as sequence labeling
+        self.assertEqual('ner', pipelines[2][0])    # Named Entity Recognitio
+        self.assertEqual('CompoundSplitter', pipelines[3][0])
+
+    def test_analyze(self):
         p = Processing()
-        self.assertEqual('', p.example())
+        ret = p.analyze("今日の午後は、突然降り出した雨の影響で、予定が大幅に狂ってしまった。")
+        i = 0
+        map = {}
+        for token in ret:
+            tokenset = {'text': token.text, 'pos_': token.pos_, 'dep_':token.dep_ }
+            map[str(i)] = tokenset
+            i += 1
+        return map
+        print(ret)
+        self.assertEqual(23, len(ret))
 
 
 if __name__ == '__main__':
